950181a8e1756bd4cfaaa6715ca9dc1e4e705c5f
phuhl <git@ph-uhl.com>
2022-09-27 12:31:57 +0200

    Adds section and endpoint filters to api docs

--- src/apiDocs/index.ts
+++ src/apiDocs/index.ts
@@ -57,6 +57,7 @@ const getRoutes = (app: Application) => {
 
 type ApiSection = {
   title: string;
+  id?: string;
   description?: string;
   subsections?: ApiSection[];
 };
@@ -68,11 +69,13 @@ type ExtendedExpressApp = Application & {
 export const section = ({
   app,
   title,
+  id,
   description,
   routes,
 }: {
   app: Application;
   title: string;
+  id?: string;
   description?: string;
   routes?: (app: Application) => void;
 }) => {
@@ -93,6 +96,7 @@ export const section = ({
   exdendedApp.appartsApiSections = prevSections;
   exdendedApp.appartsApiSections.push({
     title,
+    id,
     description,
     subsections: addedSections,
   });
@@ -109,10 +113,48 @@ export const section = ({
   });
 };
 
-export const getApi = (app: Application) => {
+type ApiDocOptions = {
+  routesFilter?: (route) => boolean;
+  sectionFilter?: (section) => boolean;
+};
+
+const filterSections = (
+  sections: ApiSection[],
+  sectionFilter: (s: ApiSection) => boolean,
+  oldSectionPrefix = "",
+  newSectionPrefix = ""
+) => {
+  const filteredSections: ApiSection[] = [];
+  let indexReplacements = {};
+  let counter = 0;
+  for (const section of sections) {
+    const filterResult = sectionFilter(section);
+    const {
+      filteredSections: subsections,
+      indexReplacements: subsectionReplacements,
+    } = filterSections(
+      section.subsections,
+      filterResult ? () => true : sectionFilter,
+      counter + ".",
+      filteredSections.length + "."
+    );
+    indexReplacements = { ...indexReplacements, ...subsectionReplacements };
+    if (subsections.length > 0 || filterResult) {
+      indexReplacements[oldSectionPrefix + counter] =
+        newSectionPrefix + filteredSections.length;
+      filteredSections.push({ ...section, subsections: subsections });
+    } else {
+      indexReplacements[oldSectionPrefix + counter] = false;
+    }
+    counter++;
+  }
+  return { filteredSections, indexReplacements };
+};
+
+export const getApi = (app: Application, options: ApiDocOptions) => {
   const exdendedApp = app as ExtendedExpressApp;
 
-  const api = getRoutes(app).map(
+  let routes = getRoutes(app).map(
     ({ method, path, assertions, returns, title, description, options }) => ({
       method,
       path,
@@ -123,5 +165,46 @@ export const getApi = (app: Application) => {
       options,
     })
   );
-  return { routes: api, sections: exdendedApp.appartsApiSections || [] };
+  if (options?.routesFilter) {
+    routes = routes.filter(options.routesFilter);
+  }
+  const originalSections = exdendedApp.appartsApiSections || [];
+  let sections = originalSections;
+  let sectionMap: Record<string, string | false> = {};
+  if (options?.sectionFilter) {
+    const { filteredSections, indexReplacements } = filterSections(
+      originalSections,
+      options.sectionFilter
+    );
+    sections = filteredSections;
+    sectionMap = indexReplacements;
+  }
+
+  for (const key in sectionMap) {
+    const newId = sectionMap[key];
+
+    routes = routes.filter(
+      (route) =>
+        !(route.options?.section && route.options.section === key) ||
+        newId !== false
+    );
+  }
+
+  for (const key in sectionMap) {
+    const newId = sectionMap[key];
+
+    routes = routes.map((route) => {
+      if (route.options?.section && route.options.section === key) {
+        return {
+          ...route,
+          options: {
+            ...route.options,
+            section: newId,
+          },
+        };
+      }
+      return route;
+    });
+  }
+  return { routes, sections };
 };
